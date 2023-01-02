b651d7909d7980df02583f8d23c7743a81369547
ideagenJack <jack.neville@ideagen.com>
2023-01-02 10:43:45 +0000

    First commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+  <div className="jumbotron">
+    <h1>Jack&#39;s Books</h1>
+    <p>Create your book library</p>
+  </div>
+);
+
+export default HomePage;
